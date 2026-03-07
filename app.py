abf079cdb7e496e2d981ffd8c499205277c4e550
yousif-saif <mrdento82@gmail.com>
2026-03-07 17:00:09 +0300

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, StreamingResponse
 from pydantic import BaseModel
 import yt_dlp
+import uvicorn
+import asyncio
+import json
+import os
+import io
 
 app = FastAPI()
 
@@ -12,15 +18,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class URLRequest(BaseModel):
-    url: str
+def load_file(filename):
+    with open(filename, "rb") as f:
+        return f.readlines()
 
+
+class URLRequest(BaseModel):
+    id: str | None = None
+    title: str | None = None
+    url: str
+        
+ydl_opts = {'quiet': True, 'no_warnings': True}
 @app.post("/analyze")
 async def analyze_video(request: URLRequest):
-    ydl_opts = {'quiet': True, 'no_warnings': True}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
-            info = ydl.extract_info(request.url, download=False)
+            info = ydl.extract_info(request.url, download=False)            
             formats = []
             for f in info.get('formats', []):
                 if f.get('height'):
@@ -28,17 +41,49 @@ async def analyze_video(request: URLRequest):
                         "id": f['format_id'],
                         "ext": f['ext'],
                         "quality": f'{f["height"]}p',
-                        "url": f.get('url')
+                        "url": f.get('url'),
+                        "title": info.get("title")
                     })
             return {
                 "title": info.get('title'),
                 "thumbnail": info.get('thumbnail'),
-                "formats": formats[-5:] # آخر 5 جودات متاحة
+                "formats": formats[-5:]
             }
         except Exception as e:
             raise HTTPException(status_code=400, detail="الرابط غير مدعوم أو خاص")
+        
+        
+        
 
-@app.get("/")
+
+@app.post("/download_video")
+async def downalod_video(request: URLRequest):
+    try:    
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([request.url])
+
+        return {
+            "finished": True
+        }
+        
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Failed to download")
+
+    
+
+    
+@app.post("/langs")
+async def send_langs():
+    langs = load_file("./langs.json")
+    return json.loads(b"\n".join(langs))
+
+
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"status": "Online"}
+    html_file_content = load_file("./index.html")    
+    return b"\n".join(html_file_content)
 
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="192.168.31.7")
